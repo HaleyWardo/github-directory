84cecf3478500b05148a02e2c397e9ea64968e16
Haley Ward <hayevans18@gmail.com>
2018-08-21 11:00:44 -0500

    handleSearch method added
    handleSearch method added to request information from GitHub API depending on user input.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const directory = new Vue({
   el: '#app',
   data: {
-    heading: 'GitHub Directory'
+    heading: 'GitHub Directory',
+    query: '',
+    info: []
   },
   methods: {
-    handleFetch: function() {
+    handleSearch: function() {
+      this.query = event.target.previousElementSibling.value
 
+      axios
+      .get(`https://api.github.com/search/users?q=${this.query}`)
+      // .get(`https://api.github.com/users/${this.query.login}/repos`)
+      .then(response => {
+        this.info = response.data.items;
+      })
     }
-  }
+  },
 });
+
+
+// TODO:
+// * make sure input value is either uppercase or lowercase
+// * if no results are found - create error message
